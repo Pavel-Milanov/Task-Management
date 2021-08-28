b877855d2f6b62d3eb4bbd39e3a9491c125231c3
raliparash <ralitsapavlova@abv.bg>
2021-08-28 20:13:52 +0300

    Implement ChangeBugStatusCommand_Test.

--- tasks-management/tests/com/taskmanagement/commands/ChangeBugStatusCommand_Test.java
+++ tasks-management/tests/com/taskmanagement/commands/ChangeBugStatusCommand_Test.java
@@ -1,4 +1,75 @@
 package com.taskmanagement.commands;
 
+import com.taskmanagement.commands.contracts.Command;
+import com.taskmanagement.commands.creation.change.ChangeBugStatusCommand;
+import com.taskmanagement.core.TaskManagementHelperRepositoryImpl;
+import com.taskmanagement.core.TaskManagementRepositoryImpl;
+import com.taskmanagement.core.contacts.TaskManagementRepository;
+import com.taskmanagement.exceptions.ElementNotFoundException;
+import com.taskmanagement.models.enums.BugStatus;
+import com.taskmanagement.models.enums.Priority;
+import com.taskmanagement.models.enums.Severity;
+import com.taskmanagement.utils.TestUtilities;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
 public class ChangeBugStatusCommand_Test {
+
+    private TaskManagementRepository taskManagementRepository;
+    private TaskManagementHelperRepositoryImpl helperRepository;
+    private Command command;
+
+    @BeforeEach
+    public void before() {
+        this.taskManagementRepository = new TaskManagementRepositoryImpl();
+        this.command = new ChangeBugStatusCommand(taskManagementRepository);
+        this.helperRepository = new TaskManagementHelperRepositoryImpl(taskManagementRepository);
+    }
+
+    @ParameterizedTest(name = "with arguments count: {0}")
+    @ValueSource(ints = {ChangeBugStatusCommand.EXPECTED_NUMBER_OF_ARGUMENTS - 1,ChangeBugStatusCommand.EXPECTED_NUMBER_OF_ARGUMENTS + 1})
+    public void execute_should_throwException_when_argumentsCountDifferentThanExpected(int argumentsCount) {
+        // Arrange
+        List<String> arguments = TestUtilities.initializeListWithSize(argumentsCount);
+
+        // Act, Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> command.executeCommand(arguments));
+    }
+
+    @Test
+    public void execute_should_throwException_when_passInvalidBugStatus() {
+
+        // Arrange
+        List<String> parameters = List.of(String.valueOf(1), "Medium");
+
+        // Act, Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> command.executeCommand(parameters));
+    }
+
+    @Test
+    public void execute_should_throwException_when_passInvalidId() {
+        // Arrange
+        int bugId = 1;
+        // Act, Assert
+        Assertions.assertThrows(ElementNotFoundException.class, () -> helperRepository.findElementById(taskManagementRepository.getBugs(), bugId));
+    }
+
+    @Test
+    public void execute_should_changeBugStatus_when_argumentIsValid() {
+
+        //Arrange
+        taskManagementRepository.createBug("The program freezes", "This needs to be fixed quickly!", Priority.HIGH, Severity.CRITICAL, BugStatus.ACTIVE, "Peter");
+        List<String> parameters = List.of(String.valueOf(1), "fixed");
+        // Act
+        command.executeCommand(parameters);
+
+        //Assert
+        Assertions.assertTrue(() -> taskManagementRepository.getBugs().get(0).getBugStatus().equals(BugStatus.FIXED));
+
+    }
 }
