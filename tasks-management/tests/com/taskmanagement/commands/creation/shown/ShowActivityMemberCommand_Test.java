4a28ecf1b7564884c6831c980ff8845a5837eab9
raliparash <ralitsapavlova@abv.bg>
2021-08-29 16:28:03 +0300

    Add and implement ShowActivityMemberCommand_Test.java

---
+++ tasks-management/tests/com/taskmanagement/commands/creation/shown/ShowActivityMemberCommand_Test.java
@@ -0,0 +1,60 @@
+package com.taskmanagement.commands.creation.shown;
+
+import com.taskmanagement.commands.contracts.Command;
+import com.taskmanagement.core.TaskManagementHelperRepositoryImpl;
+import com.taskmanagement.core.TaskManagementRepositoryImpl;
+import com.taskmanagement.core.contacts.TaskManagementRepository;
+import com.taskmanagement.models.contracts.ActivityHistory;
+import com.taskmanagement.models.contracts.Board;
+import com.taskmanagement.models.contracts.Member;
+import com.taskmanagement.utils.ListingHelpers;
+import com.taskmanagement.utils.TestUtilities;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Locale;
+
+public class ShowActivityMemberCommand_Test {
+
+    private TaskManagementRepository taskManagementRepository;
+    private TaskManagementHelperRepositoryImpl helperRepository;
+    private Command command;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-yyyy HH:mm:ss", Locale.US);
+    private LocalDateTime timestamp = LocalDateTime.now();
+
+    @BeforeEach
+    public void before() {
+        this.taskManagementRepository = new TaskManagementRepositoryImpl();
+        this.command = new ShowActivityMemberCommand(taskManagementRepository);
+        this.helperRepository = new TaskManagementHelperRepositoryImpl(taskManagementRepository);
+    }
+
+    @ParameterizedTest(name = "with arguments count: {0}")
+    @ValueSource(ints = {ShowActivityMemberCommand.EXPECTED_NUMBER_OF_ARGUMENTS + 1})
+    public void execute_should_throwException_when_argumentsCountDifferentThanExpected(int argumentsCount) {
+        // Arrange
+        List<String> arguments = TestUtilities.initializeListWithSize(argumentsCount);
+
+        // Act, Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> command.executeCommand(arguments));
+    }
+
+    @Test
+    public void execute_should_showActivityBoard_when_passedValidInput() {
+
+        Member member = taskManagementRepository.createMember("Peter");
+        List<ActivityHistory> activityHistories = member.getActivityHistories();
+        Assertions.assertEquals(ListingHelpers.elementsToString(activityHistories)
+                ,command.executeCommand(List.of(String.valueOf(member.getId()))));
+
+    }
+
+
+}
+
