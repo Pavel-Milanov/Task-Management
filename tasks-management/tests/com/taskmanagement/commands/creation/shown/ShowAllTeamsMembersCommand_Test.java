585024f71bb145869ecbd2abbcfcb4c0475037e4
raliparash <ralitsapavlova@abv.bg>
2021-08-29 18:01:11 +0300

    Add and implement ShowAllTeamsMembersCommand_Test.

--- tasks-management/tests/com/taskmanagement/commands/creation/shown/ShowAllTeamsMembersCommand_Test.java
+++ tasks-management/tests/com/taskmanagement/commands/creation/shown/ShowAllTeamsMembersCommand_Test.java
@@ -1,4 +1,66 @@
 package com.taskmanagement.commands.creation.shown;
 
+import com.taskmanagement.commands.contracts.Command;
+import com.taskmanagement.core.TaskManagementHelperRepositoryImpl;
+import com.taskmanagement.core.TaskManagementRepositoryImpl;
+import com.taskmanagement.core.contacts.TaskManagementRepository;
+import com.taskmanagement.models.contracts.Member;
+import com.taskmanagement.models.contracts.Team;
+import com.taskmanagement.utils.ListingHelpers;
+import com.taskmanagement.utils.TestUtilities;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 public class ShowAllTeamsMembersCommand_Test {
+
+    private TaskManagementRepository taskManagementRepository;
+    private TaskManagementHelperRepositoryImpl helperRepository;
+    private Command command;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-yyyy HH:mm:ss", Locale.US);
+    private LocalDateTime timestamp = LocalDateTime.now();
+
+    @BeforeEach
+    public void before() {
+        this.taskManagementRepository = new TaskManagementRepositoryImpl();
+        this.command = new ShowAllTeamMembersCommand(taskManagementRepository);
+        this.helperRepository = new TaskManagementHelperRepositoryImpl(taskManagementRepository);
+    }
+
+    @ParameterizedTest(name = "with arguments count: {0}")
+    @ValueSource(ints = {ShowAllTeamMembersCommand.EXPECTED_NUMBER_OF_ARGUMENTS + 1,ShowAllTeamMembersCommand.EXPECTED_NUMBER_OF_ARGUMENTS - 1})
+    public void execute_should_throwException_when_argumentsCountDifferentThanExpected(int argumentsCount) {
+        // Arrange
+        List<String> arguments = TestUtilities.initializeListWithSize(argumentsCount);
+
+        // Act, Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> command.executeCommand(arguments));
+    }
+
+    @Test
+    public void execute_should_showAllTeamsMembers_when_passedValidInput() {
+
+        //Arrange
+        Member member = taskManagementRepository.createMember("Peter");
+        //Act
+        Team team = taskManagementRepository.createTeam("Team11");
+
+        team.addMember(member);
+        List<Member> members = team.getMembers();
+        //Assert
+        Assertions.assertEquals(ListingHelpers.elementsToString(members)
+                , command.executeCommand(List.of("Team11")));
+
+    }
+
+
 }
+
